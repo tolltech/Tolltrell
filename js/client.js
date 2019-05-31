a643f2bc930e64aaa99fc474e5c79cc20a3a228f
tolltech <alexandrovpe@gmail.com>
2019-05-31 18:54:58 +0400

    use trello client.js

--- js/client.js
+++ js/client.js
@@ -442,3 +442,22 @@ TrelloPowerUp.initialize({
 });
 
 console.log('Loaded by: ' + document.referrer);
+
+var authenticationSuccess = function() {
+  console.log('Successful authentication');
+};
+
+var authenticationFailure = function() {
+  console.log('Failed authentication');
+};
+
+window.Trello.authorize({
+  type: 'popup',
+  name: 'Getting Started Application',
+  scope: {
+    read: 'true',
+    write: 'false' },
+  expiration: 'never',
+  success: authenticationSuccess,
+  error: authenticationFailure
+});
