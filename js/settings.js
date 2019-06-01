62eb26bfff12d064d79f58d81ca6d84316f13197
tolltech <alexandrovpe@gmail.com>
2019-06-01 20:20:42 +0400

    strange bug

--- js/settings.js
+++ js/settings.js
@@ -25,35 +25,32 @@ t.render(function () {
     })
 });
 
+function GetUrlParam(partamName) {
+  var url = window.location.search.substring(1);
+  var urlParams = url.split('&');
+  for (var i = 0; i < urlParams.length; ++i) {
+    var name = urlParams[i].split('=');
+    if (name[0] == partamName) {
+      return name[1];
+    }
+  }
+}
+
 document.getElementById('save').addEventListener('click', function () {
 
   //https://api.trello.com/1/cards/id/actions
 
-  function GetURLParameter(sParam)
-  {
-    var sPageURL = window.location.search.substring(1);
-    var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++)
-    {
-      var sParameterName = sURLVariables[i].split('=');
-      if (sParameterName[0] == sParam)
-      {
-        return sParameterName[1];
-      }
-    }
-  }​
-
-  var cardId = GetURLParameter('cardId');
+  var cardId = GetUrlParam('cardId');
 
   console.log('getting actions of acardId' + cardId);
 
   var s = window.Trello.get('/cards/' + cardId + '/actions')
-  .then(function(s1,s2,s3){
-    console.log('get actions');
-    console.log(s1);
-    console.log(s2);
-    console.log(s3);
-  });
+    .then(function (s1, s2, s3) {
+      console.log('get actions');
+      console.log(s1);
+      console.log(s2);
+      console.log(s3);
+    });
 
   return t.set('board', 'private', 'vegetable', vegetableSelector.value)
     .then(function () {
