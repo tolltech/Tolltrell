1ae750fc8421c12908f20d897d83a5309f7fffb9
tolltech <alexandrovpe@gmail.com>
2019-06-03 18:02:31 +0400

    sample of list changing action

--- js/client.js
+++ js/client.js
@@ -4,6 +4,54 @@ var Promise = TrelloPowerUp.Promise;
 
 var GRAY_ICON = './images/icon-gray.svg';
 
+// {
+//   "id": "58611f2931f777456e7206af",
+//   "idMemberCreator": "50054988ea13d8071f28a3e8",
+//   "data": {
+//     "listAfter": {
+//       "name": "released",
+//       "id": "56dfd7278eb32e245d4b3272"
+//     },
+//     "listBefore": {
+//       "name": "bugs",
+//       "id": "56d548991f3b41ab96396c27"
+//     },
+//     "board": {
+//       "shortLink": "PH23sklp",
+//       "name": "Tollrech",
+//       "id": "56d54894534aef0454465578"
+//     },
+//     "card": {
+//       "shortLink": "bbE2vtLa",
+//       "idShort": 5,
+//       "name": "дляфабрикметодбыстройинициализации",
+//       "id": "57d8f27a96b27161f1c01d11",
+//       "idList": "56dfd7278eb32e245d4b3272"
+//     },
+//     "old": {
+//       "idList": "56d548991f3b41ab96396c27"
+//     }
+//   },
+//   "type": "updateCard",
+//   "date": "2016-12-26T13: 46: 17.186Z",
+//   "limits": {
+    
+//   },
+//   "memberCreator": {
+//     "id": "50054988ea13d8071f28a3e8",
+//     "avatarHash": "68be867b91412ee0749c1832b4ce4f1e",
+//     "avatarUrl": "https: //trello-avatars.s3.amazonaws.com/68be867b91412ee0749c1832b4ce4f1e",
+//     "fullName": "PavelAleksandrov",
+//     "idMemberReferrer": "4f4e2b75d22354721281d39a",
+//     "initials": "PA",
+//     "nonPublic": {
+      
+//     },
+//     "nonPublicAvailable": false,
+//     "username": "pavelaleksandrov"
+//   }
+// }
+
 var getBadges = function(t){
   return t.card('id')
   .get('id')
