e17efdba7a71c25b5a0a072870473e8350a51521
tolltech <alexandrovpe@gmail.com>
2019-06-03 18:28:50 +0400

    days left on card badge

--- js/client.js
+++ js/client.js
@@ -2,76 +2,40 @@
 
 var Promise = TrelloPowerUp.Promise;
 
-var GRAY_ICON = './images/icon-gray.svg';
+var TOLLTECHER_ICON = './images/icon.png';
 
-// {
-//   "id": "58611f2931f777456e7206af",
-//   "idMemberCreator": "50054988ea13d8071f28a3e8",
-//   "data": {
-//     "listAfter": {
-//       "name": "released",
-//       "id": "56dfd7278eb32e245d4b3272"
-//     },
-//     "listBefore": {
-//       "name": "bugs",
-//       "id": "56d548991f3b41ab96396c27"
-//     },
-//     "board": {
-//       "shortLink": "PH23sklp",
-//       "name": "Tollrech",
-//       "id": "56d54894534aef0454465578"
-//     },
-//     "card": {
-//       "shortLink": "bbE2vtLa",
-//       "idShort": 5,
-//       "name": "дляфабрикметодбыстройинициализации",
-//       "id": "57d8f27a96b27161f1c01d11",
-//       "idList": "56dfd7278eb32e245d4b3272"
-//     },
-//     "old": {
-//       "idList": "56d548991f3b41ab96396c27"
-//     }
-//   },
-//   "type": "updateCard",
-//   "date": "2016-12-26T13: 46: 17.186Z",
-//   "limits": {
-    
-//   },
-//   "memberCreator": {
-//     "id": "50054988ea13d8071f28a3e8",
-//     "avatarHash": "68be867b91412ee0749c1832b4ce4f1e",
-//     "avatarUrl": "https: //trello-avatars.s3.amazonaws.com/68be867b91412ee0749c1832b4ce4f1e",
-//     "fullName": "PavelAleksandrov",
-//     "idMemberReferrer": "4f4e2b75d22354721281d39a",
-//     "initials": "PA",
-//     "nonPublic": {
-      
-//     },
-//     "nonPublicAvailable": false,
-//     "username": "pavelaleksandrov"
-//   }
-// }
-
-var getBadges = function(t){
+var getBadges = function (t) {
   return t.card('id')
-  .get('id')
-  .then(function(cardId){
-    console.log('We just loaded the card name and id for fun: ' + cardId);
+    .get('id')
+    .then(function (cardId) {
+      console.log('Getting card info for: ' + cardId);
 
-    return [{      
-      title: 'Detail Badge', // for detail badges only
-      text: 'Static',
-      icon: GRAY_ICON, // for card front badges only
-      color: null
-    }];
-  });
+      var cardInfo = await window.Trello.get('/cards/' + cardId); 
+
+      console.log('Get card info for: ' + cardId + ' with name ' + cardInfo.name);
+
+      var actions = await window.Trello.get('/cards/' + cardId + '/actions');
+      var lastListAction = actions.find(x => x.data && x.data.listAfter && x.date);
+
+      console.log('Find last list changing action for ' + cardInfo.name + '. ' + JSON.stringify(lastListAction));
+
+      var listTimeMiliseconds = new Date() - new Date(lastListAction.date);
+      var listTimeDays = listTimeMiliseconds / (1000 * 60 * 60 * 24);
+
+      return [{
+        title: 'Days left',
+        text: listTimeDays + ' Days',
+        icon: TOLLTECHER_ICON, 
+        color: null
+      }];
+    });
 };
 
 TrelloPowerUp.initialize({
-  'card-badges': function(t, options){
+  'card-badges': function (t, options) {
     return getBadges(t);
   },
-  'card-detail-badges': function(t, options) {
+  'card-detail-badges': function (t, options) {
     return getBadges(t);
   },
 
@@ -84,22 +48,22 @@ TrelloPowerUp.initialize({
       2. what to do when a user isn't completely authorized
       
   */
-  'authorization-status': function(t, options){
+  'authorization-status': function (t, options) {
     return t.get('member', 'private', 'token')
-    .then(function(token){
-      if(token){
-        return { authorized: true };
-      }
-      return { authorized: false };
-    });
+      .then(function (token) {
+        if (token) {
+          return { authorized: true };
+        }
+        return { authorized: false };
+      });
   },
-  'show-authorization': function(t, options){
+  'show-authorization': function (t, options) {
     let trelloAPIKey = '9b4a96d318dd1d5e851440173c2074fc';
 
     if (trelloAPIKey) {
       return t.popup({
         title: 'My Auth Popup',
-        args: { apiKey: trelloAPIKey }, 
+        args: { apiKey: trelloAPIKey },
         url: './authorize.html',
         height: 140,
       });
