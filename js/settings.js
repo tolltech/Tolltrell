ff6a1c69308d3385c11a27df3a6a1e61e5e5ab9c
tolltech <alexandrovpe@gmail.com>
2019-06-11 17:52:49 +0400

    aggregate info

--- js/settings.js
+++ js/settings.js
@@ -14,6 +14,21 @@ function GetUrlParam(partamName) {
     }
 }
 
+function AddTable(actionInfos, htmlId) {
+    var table = $('<table>').addClass('foo');
+    var th = $('<tr>');
+    var tr = $('<tr>');
+    for (var i = 0; i < actionInfos.length; ++i) {
+
+        th.append($('<td>').text(actionInfos[i].Name));
+        tr.append($('<td>').text(Math.floor(actionInfos[i].Days * 100) / 100.0));
+    }
+
+    table.append(th);
+    table.append(tr);
+    $('#' + htmlId).replaceWith(table);
+}
+
 t.render(async function () {
     var cardId = GetUrlParam('cardId');
 
@@ -31,38 +46,27 @@ t.render(async function () {
     }
 
     var actionInfos = await BuildActionInfosByCard(card);
+    var sumActionInfos = sumDays(actionInfos);
 
     var ctx = document.getElementById('cardLifestyleCanvas').getContext('2d');
     var chart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: actionInfos.map(x => x.Name),
+            labels: sumActionInfos.map(x => x.Name),
             datasets: [{
                 backgroundColor: 'rgb(24, 249, 114)',
                 borderColor: 'rgb(24, 249, 114)',
                 label: 'days',
-                data: actionInfos.map(x => x.Days)
+                data: sumActionInfos.map(x => x.Days)
             }]
         },
 
         // Configuration options go here
         options: {}
-    });
+    });    
 
-    var table = $('<table>').addClass('foo');
-    var th = $('<tr>');
-    var tr = $('<tr>');
-    for (var i = 0; i < actionInfos.length; ++i) {
-
-        th.append($('<td>').text(actionInfos[i].Name));
-        tr.append($('<td>').text(Math.floor(actionInfos[i].Days)));
-
-        currentDate = actionInfos[i].Date;
-    }
-
-    table.append(th);
-    table.append(tr);
-    $('#cardLifestyle').replaceWith(table);
+    AddTable(sumActionInfos, 'cardLifestyleSum');
+    AddTable(actionInfos, 'cardLifestyle');
 
     console.log('Rendered popup for card ' + cardId);
 });
