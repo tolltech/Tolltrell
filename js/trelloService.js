be958fe354a48d1bac7905b348bf66fc9b1a071a
tolltech <alexandrovpe@gmail.com>
2019-06-27 09:13:31 +0400

    Revert "add deleted cards to list report"
    This reverts commit aaccacb0027ff7099ea91280ed4dba75133f05a4.

--- js/trelloService.js
+++ js/trelloService.js
@@ -23,7 +23,7 @@ async function GetBoardCardActions(boardId) {
 }
 
 async function GetBoardLists(boardId) {
-  return await window.Trello.get('/boards/' + boardId + '/lists?cards=all');
+  return await window.Trello.get('/boards/' + boardId + '/lists?cards=open');
 }
 
 async function GetTrelloList(listId) {
